49a770cbba54466931592bdd2bf45d1b82403f1c
runoob <378823123@qq.com>
2018-02-28 18:02:28 +0800

    springboot_init
    2小时学会springboot
    https://www.imooc.com/learn/767

---
+++ demobak/src/main/java/com/example/demobak/DemobakApplication.java
@@ -0,0 +1,12 @@
+package com.example.demobak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobakApplication.class, args);
+	}
+}
---
+++ demobak/src/test/java/com/example/demobak/DemobakApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demobak;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemobakApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("test 000000");
+	}
+
+}
